449673ee9a20a01d4d7317265cf6abf2ae818fc1
Miguelangel-lf <155157447+Miguelangel-lf@users.noreply.github.com>
2024-04-07 15:24:06 +0000

    miguelLopezTodoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,81 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [inputValue, setInputValue] = useState("");
+    const [categoria, setCategoria] = useState("trabajo");
+    const [fecha, setFecha] = useState(""); 
+    const [todos, setTodos] = useState([]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleCategoriaChange = (e) => {
+        setCategoria(e.target.value);
+    };
+
+    const handleFechaChange = (e) => {
+        setFecha(e.target.value);
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter" && inputValue.trim() !== "") {
+            addTodo();
+        }
+    };
+
+    const addTodo = () => {
+        if (inputValue.trim() !== "") {
+            setTodos([...todos, { tarea: inputValue, categoria, fecha }]);
+            setInputValue("");
+            setFecha(""); 
+        }
+    };
+
+    const handleDelete = (index) => {
+        setTodos(todos.filter((todo, currentIndex) => index !== currentIndex));
+    };
+
+    return (
+        <div className="container">
+            <h1 className='titulo'>MI LISTA DE TAREAS</h1>
+            <div>
+                <select value={categoria} onChange={handleCategoriaChange}>
+                    <option value="trabajo">Trabajo</option>
+                    <option value="familia">Familia</option>
+                    <option value="casa">Casa</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="finanzas">Finanzas</option>
+                </select>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Escribe tu tarea..."
+                />
+                <input
+                    type="date"
+                    value={fecha}
+                    onChange={handleFechaChange}
+                />
+                <button className='boton' onClick={addTodo}><i class="fa-solid fa-arrow-down"></i></button>
+            </div>
+            <div className='tareas-container'>
+                <ul className='tareas'>
+                    {todos.map((todo, index) => (
+                        <li key={index} className="tarea-item">
+                            <i class="fa-solid fa-trash" onClick={() => handleDelete(index)} ></i>
+                            <span>{todo.tarea}</span>
+                            <span className="categoria">{todo.categoria}</span>
+                            <span className="fecha">{todo.fecha}</span> {}
+                        </li>
+                    ))}
+                </ul>
+            </div >
+            <div>{todos.length} tareas</div>
+        </div >
+    );
 };
 
 export default Home;
